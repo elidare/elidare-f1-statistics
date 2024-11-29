6f5b69bbbf0615982db571bcb599d1960d6b8681
elidare <elidare.jjj@gmail.com>
2024-11-29 22:04:51 +0200

    Add Map.css

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import worldData from '../assets/countries-110m.json';
+import './Map.css';
 
 const WorldMap = ({ points }) => {
     const svgRef = useRef();
@@ -42,59 +43,50 @@ const WorldMap = ({ points }) => {
             .data(topojson.feature(worldData, worldData.objects.countries).features)
             .join("path")
             .attr("d", path)
-            .attr("fill", "rgba(159, 232, 252, 0.6)")
-            .attr("stroke", "rgba(109, 182, 202, 0.8)");
+            .attr("class", "map");
 
-        const tooltip = d3.select(tooltipRef.current)
-            .style("position", "absolute")
-            .style("padding", "5px")
-            .style("background", "rgba(0, 0, 0, 0.7)")
-            .style("color", "white")
-            .style("border-radius", "4px")
-            .style("pointer-events", "none")
-            .style("opacity", 0);
+        const tooltip = d3.select(tooltipRef.current).attr("class", "tooltip");
 
-        if (points) {
-            // Draw points
-            g
-              .append("g")
-              .selectAll("g")
-              .data(points)
-              .join("g")
-              .attr("transform", d => `translate(${projection([d.long, d.lat])[0]}, ${projection([d.long, d.lat])[1]})`)
-              .each(function (d) {
-                  const group = d3.select(this);
+        // Draw points
+        g
+            .append("g")
+            .selectAll("g")
+            .data(points)
+            .join("g")
+            .attr("transform", d => `translate(${projection([d.long, d.lat])[0]}, ${projection([d.long, d.lat])[1]})`)
+            .each(function (d) {
+                const group = d3.select(this);
 
-                  group.append("circle")
+                group
+                    .append("circle")
+                    .attr("r", 3)
+                    .attr("fill", "rgba(9, 82, 102, 1)");
+
+                group
+                    .append("circle")
                     .attr("r", 15)
-                    .attr("fill", "rgba(9, 82, 102, 0)") // Transparent fill, otherwise tooltip was disappearing in the middle of a circle
-                    .attr("stroke", "rgba(9, 82, 102, 1)")
+                    .attr("class", "circle")
                     .on("mouseover", function() {
                         d3.select(this) // 'this' refers to the current circle
-                            .attr("fill", "rgba(9, 82, 102, 0.4)"); // Change the fill color on hover
+                            .attr("class", "circle hover"); // Change the fill color on hover
                     })
                     .on("mouseout", function() {
                         d3.select(this) // 'this' refers to the current circle
-                            .attr("fill", "rgba(9, 82, 102, 0)"); // Reset the fill color when mouse leaves
-                    });;
-
-                  group.append("circle")
-                    .attr("r", 3)
-                    .attr("fill", "rgba(9, 82, 102, 1)");
-              })
-              // Add event listeners to display the tooltip
-              .on("mouseover", function(event, d) {
-                  tooltip.style("opacity", 1) // Make the tooltip visible
-                      .html(`${d.name}, ${d.country}`);
-              })
-              .on("mousemove", (event) => {
-                  tooltip.style("top", `${event.pageY - fixTop + adaptTooltip}px`) // Magic number
-                      .style("left", `${event.pageX - fixLeft + adaptTooltip}px`);
-              })
-              .on("mouseout", () => {
-                  tooltip.style("opacity", 0); // Hide the tooltip
-              });
-        }
+                          .attr("class", "circle"); // Reset the fill color when mouse leaves
+                    });
+            })
+            // Add event listeners to display the tooltip
+            .on("mouseover", function(event, d) {
+                tooltip.style("opacity", 1) // Make the tooltip visible
+                    .html(`${d.name}, ${d.country}`);
+            })
+            .on("mousemove", (event) => {
+                tooltip.style("top", `${event.pageY - fixTop + adaptTooltip}px`) // Magic number
+                    .style("left", `${event.pageX - fixLeft + adaptTooltip}px`);
+            })
+            .on("mouseout", () => {
+                tooltip.style("opacity", 0); // Hide the tooltip
+            });
 
         // Define zoom behavior
         const zoom = d3.zoom()
